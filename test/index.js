327ee9c2c1355061c25bab3b2f151de08510baf6
Jake Lambert <jake@stream.me>
2016-12-15 13:30:38 -0600

    tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,17 @@
-/* global describe, it */
+/* global describe, it, beforeEach, afterEach */
 import assert from 'assert';
 import SimpleTestServer from '../';
-import http from 'http';
 import url from 'url';
 import request from 'request';
 
 describe('SimpleTestServer', function () {
 	let server;
-	it('should should load the SimpleTestServer class', function () {
-		assert.ok(SimpleTestServer, 'SimpleTestServer class loaded');
+	beforeEach(function (done) {
 		server = new SimpleTestServer();
-		assert.ok(server, 'server initiated');
-	});
-	it('should should start the server', function (done) {
 		server.start(done);
+	});
+	afterEach(function (done) {
+		server.stop(done);
 	});
 	it('should register the port', function () {
 		assert(typeof server.port === 'number', 'server.port is a number');
@@ -47,46 +45,33 @@ describe('SimpleTestServer', function () {
 		const queryValue = 'findme';
 		const httpOpts = {
 			method: 'POST',
-			port: server.port,
-			path: url.format({
+			uri: url.format({
+				protocol: 'http',
+				hostname: 'localhost',
 				pathname: path,
+				port: server.port,
 				query: {
 					[queryParam]: queryValue
 				}
 			}),
+			body: reqBodyText,
 			headers: {
 				'Content-Type': 'text/plain',
 				'Content-Length': Buffer.byteLength(reqBodyText)
 			}
 		};
-		const req = http.request(httpOpts, function (res) {
-			let body = '';
-			res.on('data', function (chunk) {
-				body += chunk;
-			});
-			res.on('end', function () {
-				try {
-					assert.strictEqual(res.statusCode, 200, '200 status code');
-					assert.ok(body = JSON.parse(body), 'response body is JSON');
-					assert.strictEqual(body.method, 'POST', 'has the correct method');
-					assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
-					assert.ok(body.query.q, 'has the correct query parameter');
-					assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
-					assert.ok(body.body, 'has a request body');
-					assert.strictEqual(body.body, reqBodyText, 'has the correct request body, has:' + JSON.stringify(body));
-					done();
-				} catch (e) {
-					assert.ifError(e);
-					done();
-				}
-			});
-		});
-		req.on('err', function (e) {
-			assert.ifError(e, 'error sending the request');
+		request(httpOpts, function (err, resp, body) {
+			assert.ifError(err);
+			assert.ok(resp.statusCode === 200, 'response is a 200');
+			assert.ok(body = JSON.parse(body), 'response body is JSON');
+			assert.strictEqual(body.method, 'POST', 'has the correct method');
+			assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
+			assert.ok(body.query.q, 'has the correct query parameter');
+			assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
+			assert.ok(body.body, 'has a request body');
+			assert.strictEqual(body.body, reqBodyText, 'has the correct request body, has:' + JSON.stringify(body));
 			done();
 		});
-		req.write(reqBodyText);
-		req.end();
 	});
 	it('should make a POST request with JSON', function (done) {
 		const reqBody = {
@@ -97,43 +82,30 @@ describe('SimpleTestServer', function () {
 		const queryValue = 'findme';
 		const httpOpts = {
 			method: 'POST',
-			port: server.port,
-			path: `${path}?${queryParam}=${queryValue}`,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		};
-		const req = http.request(httpOpts, function (res) {
-			let body = '';
-			res.on('data', function (chunk) {
-				body += chunk;
-			});
-			res.on('end', function () {
-				try {
-					assert.strictEqual(res.statusCode, 200, '200 status code');
-					assert.ok(body = JSON.parse(body), 'response body is JSON');
-					assert.strictEqual(body.method, 'POST', 'has the correct method');
-					assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
-					assert.ok(body.query.q, 'has the correct query parameter');
-					assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
-					assert.ok(body.body, 'has a request body');
-					assert.ok(JSON.stringify(body.body), 'return request body is valid JSON');
-					assert.strictEqual(body.body.test, reqBody.test, 'has the correct request body, has:' + body.body);
-					done();
-				} catch (e) {
-					assert.ifError(e);
-					done();
+			uri: url.format({
+				protocol: 'http',
+				hostname: 'localhost',
+				pathname: path,
+				port: server.port,
+				query: {
+					[queryParam]: queryValue
 				}
-			});
-		});
-		req.on('err', function (e) {
-			assert.ifError(e, 'error sending the request');
+			}),
+			json: true,
+			body: reqBody
+		};
+		request(httpOpts, function (err, resp, body) {
+			assert.ifError(err);
+			assert.strictEqual(resp.statusCode, 200, '200 status code');
+			assert.ok(JSON.stringify(body), 'response body is JSON');
+			assert.strictEqual(body.method, 'POST', 'has the correct method');
+			assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
+			assert.ok(body.query.q, 'has the correct query parameter');
+			assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
+			assert.ok(body.body, 'has a request body');
+			assert.ok(JSON.stringify(body.body), 'return request body is valid JSON');
+			assert.strictEqual(body.body.test, reqBody.test, 'has the correct request body, has:' + body.body);
 			done();
 		});
-		req.write(JSON.stringify(reqBody));
-		req.end();
-	});
-	it('should stop the server', function (done) {
-		server.stop(done);
 	});
 });
