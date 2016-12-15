4f8b9f4ff7ad0f004af4349b34ed693108d97ea2
Jake <djakelambert@gmail.com>
2016-12-14 18:51:15 -0600

    Initial work

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# simple-test-server
+# Parrot Server
+[![js-happiness-style](https://img.shields.io/badge/code%20style-happiness-brightgreen.svg)](https://github.com/JedWatson/happiness)
+
 Simple server responds to any HTTP request with details about the request.
+
+## Install
+```javascript
+npm install @streammedev/simple-test-server;
+```
+
+## Usage
+First, import the class:
+```javascript
+import SimpleTestServer from '@streammedev/simple-test-server';
+```
+Next, instantiate it:
+```javascript
+const server = new SimpleTestServer();
+```
+Next, start it:
+```
+server.start();
+```
+Your server is now accepting requests at `http://localhost`.
+
+Once you're done, stop the server by calling `stop`, ie:
+
+```javascript
+server.stop();  
+```
+
+## API
+`constructor(port)` - Instantiates a new server instance on the specified (optional) port. Whether it is set explicitly or not, server.port will be set to the port once the server has started.
+
+`start(cb)` - Starts the server, then calls the (optional) callback.
+
+`stop(cb)` - Stops the server, then calls the (optional) callback.
+
+## CLI
+A simple cli command is available:
+
+```bash
+parrot-server --port=8080
+```
+
+## Example
+```javascript
+import SimpleTestServer from '@streammedev/simple-test-server';
+import assert from 'assert';
+import request from 'request';
+
+describe('do some test', function () {
+	let testServer;
+	before(function (done) {
+		testServer = new ApiTestServer(port);
+		testServer.start(done);
+	});
+	after(function (done) {
+		testServer.stop(done);
+	});
+  it('should get my endpoint', function(done) {
+    request('http://localhost/my-endpoint?q=findme', function (error, response, body) {
+      assert.ifError(error);
+      assert.ifError(response.statusCode !== 200);
+      assert.strictEqual(body.method, 'GET', 'it should be a GET request');
+      assert.strictEqual(body.path, 'my-endpoint', 'it should have the right path');
+      assert.strictEqual(body.query.q, 'findme', 'it should set the q query parameter');
+      })
+    })
+  })
+}
+```
+
+## Contributing
+
+Contributions are welcome.
+Please see our guidelines in [CONTRIBUTING.md](contributing.md)
---
+++ test/index.js
@@ -0,0 +1,111 @@
+/* global describe, it, beforeEach, afterEach */
+import assert from 'assert';
+import SimpleTestServer from '../';
+import url from 'url';
+import request from 'request';
+
+describe('SimpleTestServer', function () {
+	let server;
+	beforeEach(function (done) {
+		server = new SimpleTestServer();
+		server.start(done);
+	});
+	afterEach(function (done) {
+		server.stop(done);
+	});
+	it('should register the port', function () {
+		assert(typeof server.port === 'number', 'server.port is a number');
+	});
+	it('should make a GET request', function (done) {
+		let httpOpts = {
+			method: 'GET',
+			uri: url.format({
+				protocol: 'http',
+				hostname: 'localhost',
+				pathname: 'a/path',
+				port: server.port,
+				query: {q: 'findme'}
+			})
+		};
+		request(httpOpts, function (err, resp, body) {
+			assert.ifError(err);
+			assert.ok(resp.statusCode === 200, 'response is a 200');
+			assert.ok(body = JSON.parse(body), 'response body is JSON');
+			assert.strictEqual(body.method, 'GET', 'has the correct method');
+			assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
+			assert.ok(body.query.q, 'has the correct query parameter');
+			assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
+			done();
+		});
+	});
+	it('should make a POST request with plain text', function (done) {
+		const reqBodyText = 'text in the body';
+		const path = '/a/path';
+		const queryParam = 'q';
+		const queryValue = 'findme';
+		const httpOpts = {
+			method: 'POST',
+			uri: url.format({
+				protocol: 'http',
+				hostname: 'localhost',
+				pathname: path,
+				port: server.port,
+				query: {
+					[queryParam]: queryValue
+				}
+			}),
+			body: reqBodyText,
+			headers: {
+				'Content-Type': 'text/plain',
+				'Content-Length': Buffer.byteLength(reqBodyText)
+			}
+		};
+		request(httpOpts, function (err, resp, body) {
+			assert.ifError(err);
+			assert.ok(resp.statusCode === 200, 'response is a 200');
+			assert.ok(body = JSON.parse(body), 'response body is JSON');
+			assert.strictEqual(body.method, 'POST', 'has the correct method');
+			assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
+			assert.ok(body.query.q, 'has the correct query parameter');
+			assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
+			assert.ok(body.body, 'has a request body');
+			assert.strictEqual(body.body, reqBodyText, 'has the correct request body, has:' + JSON.stringify(body));
+			done();
+		});
+	});
+	it('should make a POST request with JSON', function (done) {
+		const reqBody = {
+			test: 'prap'
+		};
+		const path = '/a/path';
+		const queryParam = 'q';
+		const queryValue = 'findme';
+		const httpOpts = {
+			method: 'POST',
+			uri: url.format({
+				protocol: 'http',
+				hostname: 'localhost',
+				pathname: path,
+				port: server.port,
+				query: {
+					[queryParam]: queryValue
+				}
+			}),
+			json: true,
+			body: reqBody
+		};
+		request(httpOpts, function (err, resp, body) {
+			assert.ifError(err);
+			assert.strictEqual(resp.statusCode, 200, '200 status code');
+			assert.ok(JSON.stringify(body), 'response body is JSON');
+			assert.strictEqual(body.method, 'POST', 'has the correct method');
+			assert.strictEqual(body.path, '/a/path', 'has the correct pathname');
+			assert.ok(body.query.q, 'has the correct query parameter');
+			assert.strictEqual(body.query.q, 'findme', 'has the correct query parameter value');
+			assert.ok(body.body, 'has a request body');
+			assert.ok(JSON.stringify(body.body), 'return request body is valid JSON');
+			assert.strictEqual(body.body.test, reqBody.test, 'has the correct request body, has:' + body.body);
+			done();
+		});
+	});
+});
